f9e0dd5e1b98f30788ec53c2839d097ece28dd67
C2-YK <oioi27925957@gmail.com>
2021-11-29 15:52:13 -0700

    maze class complete

--- maze.cpp
+++ maze.cpp
@@ -2,5 +2,142 @@
 
 Maze::Maze()
 {
+    height = 10;
+    width = 10;
+    fillEdgesWithWalls();
+    map[getIndex(startPoint)] = start;
+    maxCoins = 0;
+}
 
+Maze::Maze(int h, int w){
+    height = h;
+    width = w;
+    fillEdgesWithWalls();
+    map[getIndex(startPoint)] = start;
+    maxCoins = 0;
+}
+
+Maze::Maze(int h, int w, QList<int> map){
+    this->map = map;
+    height = h;
+    width = w;
+    maxCoins = 0;
+    for(int x = 0; x < w; x++){
+        for(int y = 0; y < h; y ++){
+            if(getObject(x, y) == coin){
+                maxCoins++;
+            }
+        }
+    }
+}
+
+//edit maze methods
+bool Maze::changeStartTo(int x, int y){
+    //can not add outside of the map
+    if(x >= width || y >= height || x < 0 || y < 0){
+        return false;
+    }
+    if(getObject(startPoint.x(), startPoint.y()) == wall){
+        return false;
+    }
+    objectAt(startPoint.x(), startPoint.y()) = space;
+    startPoint = QPoint(x, y);
+    objectAt(x, y) = start;
+    return true;
+}
+
+bool Maze::addWall(int x, int y){
+    //can not add outside of the map
+    if(x >= width || y >= height || x < 0 || y < 0){
+        return false;
+    }
+    //can not add wall on start point
+    if(startPoint == QPoint(x, y)){
+        return false;
+    }
+    //if the point is coin, remove the coin from map
+    if(getObject(x, y) == coin){
+        maxCoins -= 1;
+    }
+    objectAt(x, y) = wall;
+    return true;
+}
+
+bool Maze::addCoin(int x, int y){
+    //can not add outside of the map
+    if(x >= width || y >= height || x < 0 || y < 0){
+        return false;
+    }
+    //can not add coin on wall
+    if(getObject(x, y) == wall){
+        return false;
+    }
+    objectAt(x, y) = coin;
+    maxCoins += 1;
+    return true;
+}
+
+bool Maze::addSpace(int x, int y){
+    //can not add outside of the map
+    if(x >= width || y >= height || x < 0 || y < 0){
+        return false;
+    }
+    //can not add space on edges
+    if(x == 0 || x == width - 1){
+        return false;
+    }
+    if(y == 0 || y == height - 1){
+        return  false;
+    }
+    //if the point is coin, remove the coin from map
+    if(getObject(x, y) == coin){
+        maxCoins -= 1;
+    }
+    objectAt(x, y) = space;
+    return true;
+}
+
+//for saving to jason
+QList<int> Maze::getMap(){
+    return map;
+}
+
+int Maze::getHeight(){
+    return height;
+}
+
+int Maze::getWidth(){
+    return width;
+}
+
+//helper
+int Maze::getIndex(QPoint point){
+    return point.x() + point.y() * width;
+}
+
+void Maze::fillEdgesWithWalls(){
+    //fill top
+    for(int i = 0; i < width; i ++){
+        map[i] = wall;
+    }
+    //fill bottom
+    for(int i = height * (width - 1); i < height * width; i ++){
+        map[i] = wall;
+    }
+    //fill left
+    for(int i = width; i < height * width; i += width){
+        map[i] = wall;
+    }
+    //fill right
+    for(int i = width - 1; i < height * width; i += width){
+        map[i] = wall;
+    }
+}
+
+int Maze::getObject(int x, int y){
+    return map[getIndex(QPoint(x, y))];
+}
+
+int& Maze::objectAt(int x, int y){
+    return map[getIndex(QPoint(x, y))];
 }
--- maze.h
+++ maze.h
@@ -1,11 +1,42 @@
 #ifndef MAZE_H
 #define MAZE_H
-
+#include <QList>
+#include <QPoint>
 
 class Maze
 {
 public:
     Maze();
+    //height and width must > 2
+    Maze(int h, int w);
+    //for loading form jason
+    Maze(int h, int w, QList<int> map);
+    //edit maze methods
+    bool changeStartTo(int x, int y);
+    bool addWall(int x, int y);
+    bool addCoin(int x, int y);
+    bool addSpace(int x, int y);
+    //game play method
+
+    //for saving to jason
+    QList<int> getMap();
+    int getHeight();
+    int getWidth();
+private:
+    QPoint startPoint = QPoint(1, 1);
+    int height;
+    int width;
+    int maxCoins;
+    QList<int> map;
+    int space = 0;
+    int start = 1;
+    int wall = 2;
+    int coin = 3;
+    //helper
+    void fillEdgesWithWalls();
+    int getIndex(QPoint postion);
+    int getObject(int x, int y);
+    int& objectAt(int x, int y);
 };
 
 #endif // MAZE_H
