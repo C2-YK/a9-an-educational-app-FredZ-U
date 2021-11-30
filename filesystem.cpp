a10da151877d48059af8494ff9904affa2a5ec14
kang-2020 <kangzhao2019@gmail.com>
2021-11-30 16:49:36 -0700

    write method to save maze to file
    write method to save maze to file
    have not tested

--- filesystem.cpp
+++ filesystem.cpp
@@ -4,3 +4,48 @@ FileSystem::FileSystem()
 {
 
 }
+
+bool FileSystem::loadMazeFromFile(QString filepath)
+{
+    QFile loadFile(filepath);
+    if(!loadFile.open(QIODevice::ReadOnly))
+    {
+        qWarning("Could not open the file.");
+        return false;
+    }
+
+    QByteArray saveData = loadFile.readAll();
+    QJsonDocument loadDoc(QJsonDocument::fromJson(saveData));
+    //mazeReader(loadDoc.object(),*maze);
+    return true;
+}
+
+bool FileSystem::saveMazeToFile(QString filename)
+{
+    QFile saveFile(filename);
+    if(!saveFile.open(QIODevice::WriteOnly))
+    {
+        qWarning("Could not open the file in a writing way");
+        return false;
+    }
+
+    QJsonObject mazeObject;
+    mazeWriter(mazeObject);
+    QJsonDocument saveDoc(mazeObject);
+    saveFile.write(saveDoc.toJson());
+    return true;
+}
+
+void FileSystem:: mazeWriter(QJsonObject &json)
+{
+    json["height"] = maze->getHeight();
+    json["width"] = maze->getWidth();
+    QList<int> map(maze->getMap());
+    QString mapInfo;
+    foreach(int info,map)
+    {
+        mapInfo.append(QString::number(info));
+        mapInfo.append(" ");
+    }
+    json["info"] = mapInfo;
+}
--- filesystem.h
+++ filesystem.h
@@ -2,12 +2,27 @@
 #define FILESYSTEM_H
 
 #include <QObject>
+#include <QString>
+#include <QStringList>
+#include <QRegularExpression>
+#include <QFile>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include "maze.h"
 
 class FileSystem : public QObject
 {
     Q_OBJECT
 public:
     FileSystem();
+    bool loadMazeFromFile(QString);
+    bool saveMazeToFile(QString);
+private:
+    Maze *maze;
+    void mazeWriter(QJsonObject&);
+    Maze mazeReader(QJsonObject&);
+
+
 };
 
 #endif // FILESYSTEM_H
