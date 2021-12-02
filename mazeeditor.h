166b8adb549154bf65c5cd77b4690d787e2d958d
Aidan Bott <aidanbott@gmail.com>
2021-12-01 19:50:55 -0700

    Added mazeeditor
    updated header

--- mazeeditor.h
+++ mazeeditor.h
@@ -1,19 +1,22 @@
 #ifndef MAZEEDITOR_H
 #define MAZEEDITOR_H
-
+#include "maze.h"
 #include <QObject>
 #include <QPoint>
+#include <QVector2D>
 
 class MazeEditor : public QObject
 {
     Q_OBJECT
 public:
     MazeEditor();
+    void setMaze(Maze*);
 public slots:
     void switchTool(int toolIndex);
-    void resize(int height, int width);
     void useToolOn(QPoint position);
 private:
+    Maze* target;
+    int toolIndex;
     void addWall(QPoint position);
     void addSpace(QPoint position);
     void addCoin(QPoint position);
